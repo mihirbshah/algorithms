9a94aca8ac9dc3a177f6abdd6bc9d3210796a316
Mihir Shah <maverick.mihir@gmail.com>
2020-06-11 11:29:54 -0400

    max points from cards

---
+++ src/max_points_from_cards.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+int maxScore(vector<int>& cardPoints, int k)
+{
+  int left = accumulate(cardPoints.begin(), cardPoints.begin()+k, 0);
+  int right = 0, res = left, n = cardPoints.size();
+  for (int i = 0; i < k; ++i)
+  {
+    left -= cardPoints[k - 1 - i];
+    right += cardPoints[n - 1 - i];
+    res = max(res, left + right);
+  }
+
+  return res;  
+}
+
+int main()
+{
+  vector<int> v1 = {9,7,5,3,4,5,100,7,9}; int k1 = 3;
+  int res = maxScore(v1, k1);
+  cout << "res: " << res << "\n";
+  return 0;
+}
