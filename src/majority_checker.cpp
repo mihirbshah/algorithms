618067fa0c84c77210119fa5921611ee8c8b3626
Mihir Shah <maverick.mihir@gmail.com>
2020-10-07 12:16:13 -0400

    1157. Online Majority Element In Subarray

--- src/majority_checker.cpp
+++ src/majority_checker.cpp
@@ -1,16 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
+
+/*
+https://leetcode.com/problems/online-majority-element-in-subarray/
+
+This solution is taken from https://leetcode.com/problems/online-majority-element-in-subarray/discuss/356108/C%2B%2B-160-ms-frequency-%2B-binary-search
+*/
 
 namespace
 {
     using List1D = vector<int>;
-    using List2D = vector<vector<int>>;
-    const int MAX_ELEMENT_VAL = 20000;
+    //using List2D = vector<vector<int>>;
+    using IndexMap = unordered_map<int, vector<int>>;
+    //const int MAX_ELEMENT_VAL = 20000;
 }
+class MajorityChecker
+{
+public:
+    MajorityChecker(List1D& arr) 
+    {
+        for (int i = 0; i < arr.size(); ++i)
+        {
+            indices[arr[i]].push_back(i);
+        }
+    }
+    
+    int query(int left, int right, int threshold) 
+    {
+        for (const auto& bucket : indices)
+        {
+            if (bucket.second.size() < threshold) continue;
+            auto low = lower_bound (bucket.second.begin(), bucket.second.end(), left);
+            auto high = upper_bound (bucket.second.begin(), bucket.second.end(), right);
+            if (distance(low, high) >= threshold) return bucket.first;
+        }
+        return -1;
+    }
 
-class MajorityChecker 
+private:
+    IndexMap indices;
+};
+
+/*class MajorityChecker_mihir 
 {
 public:
     MajorityChecker(List1D& arr) 
@@ -28,15 +63,6 @@ public:
     
     int query(int left, int right, int threshold) 
     {
-        /*for (int i = 0; i <= 6; ++i)
-        {
-            for (int j = 0; j <= 6; ++j)
-            {
-                cout << _prefixCounts[i][j] << ", ";
-            }
-            cout << "\n";
-        }*/
-        
         // C++2x but O(n) space
         List1D subarrCounts(MAX_ELEMENT_VAL, 0);
         std::transform(_prefixCounts[right+1].begin(), 
@@ -47,19 +73,20 @@ public:
         auto it = find_if(subarrCounts.begin(), subarrCounts.end(), [&](int e){ return e >= threshold; });
         return it == subarrCounts.end() ? -1 : distance(subarrCounts.begin(), it);
         
-        /* C++98 but O(1) space
-        const List1D& rList = _prefixCounts[right+1], lList = _prefixCounts[left];
-        for (int i = 0; i < MAX_ELEMENT_VAL; ++i)
-        {
-            if (rList[i] - lList[i] >= threshold) return i;
-        }
-        return -1;
-        */
+        //C++98 but O(1) space
+        //const List1D& rList = _prefixCounts[right+1], lList = _prefixCounts[left];
+        //for (int i = 0; i < MAX_ELEMENT_VAL; ++i)
+        //{
+        //    if (rList[i] - lList[i] >= threshold) return i;
+        //}
+        //return -1;
+        
     }
     
 private:
     List2D _prefixCounts;
 };
+*/
 
 int main()
 {
