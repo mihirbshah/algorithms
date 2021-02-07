9cf0acda009648202c7ca5138fbb7a3a31f38bcf
Mihir Shah <maverick.mihir@gmail.com>
2021-02-07 11:19:36 -0500

    1192. Critical Connections in a Network

---
+++ src/critical_connections_network.cpp
@@ -0,0 +1,85 @@
+// 1192. Critical Connections in a Network
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include "util.h"
+
+using namespace std;
+
+class Solution 
+{
+public:
+    using Graph = unordered_map<int, vector<int>>;
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) 
+    {
+        Graph graph(n);
+        for (const auto c : connections) 
+        {
+            graph[c[0]].push_back(c[1]);
+            graph[c[1]].push_back(c[0]);
+        }
+        
+        /*for (const auto& [node, edges] : graph)
+        {
+            cout << node << ": " << stringify_container(edges.begin(), edges.end()) << "\n";
+        }*/
+        
+        vector<vector<int>> critical;
+        for (const auto& [node, edges] : graph)
+        {
+            //cout << "Checking node: " << node << "\n";
+            for (const auto& edge : edges)
+            {
+                //cout << "\tChecking connection: " << edge << "\n";
+                if (isCritical(graph, node, edge) && node > edge) critical.push_back({node, edge});
+                //cout << "\tcritical size: " << critical.size() << "\n";
+            }
+        }
+        return critical;
+    }
+    
+private:
+    bool canReachTarget(Graph& graph, int edge, int target, unordered_set<int> traversed)
+    {
+        const vector<int>& edges = graph[edge];
+        if (find(edges.begin(), edges.end(), target) != edges.end()) return true;
+        traversed.insert(edge);
+        for (const auto& e : edges)
+        {
+            if (traversed.count(e) != 0) continue;
+            if (canReachTarget(graph, e, target, traversed)) return true;
+        }
+        
+        return false;
+    }
+    
+    bool isCritical(Graph& graph, int source, int target)
+    {
+        bool ret = true;
+        for (const auto& e : graph[source])
+        {
+            if (e == target) continue;
+            //cout << "\t\tInspecting edge: [" << source << ", " << e << "]\n";
+            ret &= !canReachTarget(graph, e, target, {source});
+            //cout << "\t\tret: " << (ret ? "true" : "false") << "\n";
+        }
+        
+        return ret;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> g({{0,1},{1,2},{2,0},{1,3}}); int n = 4;
+    //vector<vector<int>> g({{0,1},{1,2},{2,0},{1,3},{3,4},{4,5},{5,3}}); int n = 6;
+    Solution o;
+    vector<vector<int>> res = o.criticalConnections(n, g);
+    for (const auto& edge : res)
+    {
+        cout << "[" << edge[0] << ", " << edge[1] << "]\n";
+    }
+    return 0;
+}
