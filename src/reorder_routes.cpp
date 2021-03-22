253dae07ec753f047fdfe97882cd0d967135ed7a
Mihir Shah <maverick.mihir@gmail.com>
2021-03-22 17:28:21 -0400

    fix MLE

--- src/reorder_routes.cpp
+++ src/reorder_routes.cpp
@@ -8,8 +8,6 @@ using namespace std;
 
 /*
 https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/
-
-Note: Code runs fine on local compiler but throws heap memory exception on leetcode
 */
 
 namespace
@@ -21,15 +19,14 @@ class Solution {
 public:
     int minReorder(int n, vector<vector<int>>& connections) 
     {
-        numElems = n;
         for (auto connection : connections)
         {
             const int x = connection[0], y = connection[1];
             upsert(inbound, y, x);
             upsert(outbound, x, y);
         }
-        dfs(0);
-        return flips;
+        unordered_set<int> visited;
+        return dfs(0, visited);
     }
     
 private:
@@ -40,46 +37,30 @@ private:
         else list[x].insert(y);
     }
     
-    void dfs(int n)
+    int dfs(int n, unordered_set<int>& visited)
     {
-        cout << "node: " << n << ", flips: " << flips << "\n";
-        //cout << "connected: " << stringify_container(connected.begin(), connected.end()) << "\n";
-        if (!connected.count(n)) return;
-        if (connected.size() == numElems) return;
-        
-        if (inbound[n].size())
+        int res = 0;
+        visited.insert(n);
+        if (inbound.count(n))
         {
             for (auto e : inbound[n])
             {
-                cout << "== (inbound) node == " << n << "\n";
-                connected.insert(e);
-                dfs(e);
+                if (!visited.count(e)) res += dfs(e, visited);
             }
         }
         
-        if (outbound[n].size())
+        if (outbound.count(n))
         {
             for (auto e : outbound[n])
             {
-                cout << "== (outbound) node == " << n << "\n";
-                if (!connected.count(e))
-                {
-                    outbound[n].erase(e);
-                    inbound[e].erase(n);
-                    upsert(inbound, n, e);
-                    upsert(outbound, e, n);
-                    connected.insert(e);
-                    ++flips;
-                    dfs(e);
-                }
+                if (!visited.count(e)) res += (1 + dfs(e, visited));
             }
         }
+        visited.erase(n);
+        return res;
     }
     
     AdjList inbound, outbound;
-    unordered_set<int> connected = {0};
-    int flips = 0;
-    int numElems;
 };
 
 int main()
